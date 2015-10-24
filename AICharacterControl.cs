e6bae10d71d8c1eab2c87943d57af2eb50bf53f9
jacobhan <jungwoohoo@gmail.com>
2015-10-24 11:17:13 -0700

    Update AICharacterControl.cs

--- AICharacterControl.cs
+++ AICharacterControl.cs
@@ -6,12 +6,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
   [RequireComponent(typeof (NavMeshAgent))]
   [RequireComponent(typeof (ThirdPersonCharacter))]
   
-  public class AICharacterControl : MonoBehaviour
-  {
+  public class AICharacterControl : MonoBehaviour {
     public NavMeshAgent agent { get; private set;
     public ThirdPersonCharacter character { get; private set; }
     public Transform target;
     
+    private void Start()
   }
   
 }
