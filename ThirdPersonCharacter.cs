15f3af2ca714e757ba8e8565b532174bbfa57554
jacobhan <jungwoohoo@gmail.com>
2015-10-30 15:34:52 -0700

    Update ThirdPersonCharacter.cs

--- ThirdPersonCharacter.cs
+++ ThirdPersonCharacter.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson {
-  [RequireComponent(typeof(Rigidbody))]
-  [RequireComponent(typeof(CapsuleCollider))]
-  [RequireComponent(typeof(Animator))]
-  public class ThirdPersonCharacter : MonoBehaviour {
-    [SerializeField] float m_MovingTurnSpeed = 360;
+	[RequireComponent(typeof(Rigidbody))]
+  	[RequireComponent(typeof(CapsuleCollider))]
+  	[RequireComponent(typeof(Animator))]
+  	public class ThirdPersonCharacter : MonoBehaviour {
+    		[SerializeField] float m_MovingTurnSpeed = 360;
 		[SerializeField] float m_StationaryTurnSpeed = 180;
-  }
+  	}
 }
