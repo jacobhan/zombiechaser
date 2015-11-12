77cc46bd6bc637fb9f0331d05db7e311d515761e
jacobhan <jungwoohoo@gmail.com>
2015-11-11 17:11:39 -0800

    Update ThirdPersonCharacter.cs

--- ThirdPersonCharacter.cs
+++ ThirdPersonCharacter.cs
@@ -92,6 +92,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson {
 		void UpdateAnimator(Vector3 move) {
 			m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
 			m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
+			m_Animator.SetBool("Crouch", m_Crouching);
+			m_Animator.SetBool("OnGround", m_IsGrounded);
 		}
 		
   	}
