1194cda4ba3c9df149e1b96edff1e3df41f4c627
jacobhan <jungwoohoo@gmail.com>
2015-11-19 00:10:12 -0800

    Update ThirdPersonCharacter.cs

--- ThirdPersonCharacter.cs
+++ ThirdPersonCharacter.cs
@@ -142,7 +142,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson {
 		void CheckGroundStatus() {
 			RaycastHit hitInfo;
 			if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance)) {
-				
+				m_GroundNormal = hitInfo.normal;
+				m_IsGrounded = true;
+				m_Animator.applyRootMotion = true;
 			}
 		}
   	}
