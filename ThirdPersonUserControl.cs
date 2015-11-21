ac1ff2717162eaacd63353d6f3b65148edda37ce
jacobhan <jungwoohoo@gmail.com>
2015-11-21 01:40:32 -0800

    Update ThirdPersonUserControl.cs

--- ThirdPersonUserControl.cs
+++ ThirdPersonUserControl.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Characters.ThirdPerson {
-  
+  [RequireComponent(typeof (ThirdPersonCharacter))]
+  public class ThirdPersonUserControl : MonoBehaviour {
+    
+  }
   
 }
