ac7a4a5026894c0c64ddcbc50458655061c3679d
jacobhan <jungwoohoo@gmail.com>
2015-11-21 01:40:54 -0800

    Update ThirdPersonUserControl.cs

--- ThirdPersonUserControl.cs
+++ ThirdPersonUserControl.cs
@@ -5,7 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 namespace UnityStandardAssets.Characters.ThirdPerson {
   [RequireComponent(typeof (ThirdPersonCharacter))]
   public class ThirdPersonUserControl : MonoBehaviour {
-    
+    private ThirdPersonCharacter m_Character;
+        private Transform m_Cam;
+        private Vector3 m_CamForward;
+        private Vector3 m_Move;
+        private bool m_Jump;
   }
   
 }
