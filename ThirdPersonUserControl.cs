8e070da1c6b837ed1a91a7f6a1e96538376fc9cb
jacobhan <jungwoohoo@gmail.com>
2015-11-21 01:41:17 -0800

    Update ThirdPersonUserControl.cs

--- ThirdPersonUserControl.cs
+++ ThirdPersonUserControl.cs
@@ -6,10 +6,14 @@ namespace UnityStandardAssets.Characters.ThirdPerson {
   [RequireComponent(typeof (ThirdPersonCharacter))]
   public class ThirdPersonUserControl : MonoBehaviour {
     private ThirdPersonCharacter m_Character;
-        private Transform m_Cam;
-        private Vector3 m_CamForward;
-        private Vector3 m_Move;
-        private bool m_Jump;
+    private Transform m_Cam;
+    private Vector3 m_CamForward;
+    private Vector3 m_Move;
+    private bool m_Jump;
+    
+    private void Start() {
+      
+    }
   }
   
 }
