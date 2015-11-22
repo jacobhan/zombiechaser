1d0a0fceb03a999f1e822e21d6a5984b3e665c04
jacobhan <jungwoohoo@gmail.com>
2015-11-22 03:17:09 -0800

    Update ThirdPersonUserControl.cs

--- ThirdPersonUserControl.cs
+++ ThirdPersonUserControl.cs
@@ -13,7 +13,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson {
     
     private void Start() {
       if (Camera.main != null) {
-        
+        m_Cam = Camera.main.transform
       }
     }
   }
