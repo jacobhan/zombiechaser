5d0a5cd315de785b1a44cb9007014bfec736e511
jacobhan <jungwoohoo@gmail.com>
2015-11-30 12:20:03 -0800

    Update HeadBob.cs

--- HeadBob.cs
+++ HeadBob.cs
@@ -14,6 +14,12 @@ namespace UnityStandardAssets.Characters.FirstPerson {
     private bool m_PreviouslyGrounded;
     private Vector3 m_OriginalCameraPosition;
     
+    private void Start() {
+      
+      
+      
+    }
+    
   }
   
 }
