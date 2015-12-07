1edc2cb975d4fb013e11a9255f4f2774002ec596
jacobhan <jungwoohoo@gmail.com>
2015-12-06 18:12:02 -0800

    Update MouseLook.cs

--- MouseLook.cs
+++ MouseLook.cs
@@ -7,6 +7,10 @@ namespace UnityStandardAssets.Characters.FirstPerson {
   public class MouseLook {
     public float XSensitivity = 2f;
     public float YSensitivity = 2f;
+    public bool clampVerticalRotation = true;
+    public float MinimumX = -90F;
+    public float MaximumX = 90F;
+    
     
   }
   
