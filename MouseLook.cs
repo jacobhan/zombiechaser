522a91902d8ca7f9f976e1af41612b941f58361c
jacobhan <jungwoohoo@gmail.com>
2015-12-06 18:12:17 -0800

    Update MouseLook.cs

--- MouseLook.cs
+++ MouseLook.cs
@@ -10,7 +10,8 @@ namespace UnityStandardAssets.Characters.FirstPerson {
     public bool clampVerticalRotation = true;
     public float MinimumX = -90F;
     public float MaximumX = 90F;
-    
+    public bool smooth;
+    public float smoothTime = 5f;
     
   }
   
