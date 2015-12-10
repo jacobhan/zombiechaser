77ae1948c7a540ddbb5c22938f642d4b400f91fc
jacobhan <jungwoohoo@gmail.com>
2015-12-10 15:44:54 -0800

    Update MouseLook.cs

--- MouseLook.cs
+++ MouseLook.cs
@@ -26,6 +26,9 @@ namespace UnityStandardAssets.Characters.FirstPerson {
 
       m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
       m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+      
+      if(clampVerticalRotation)
+        m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
     }
     
   }
