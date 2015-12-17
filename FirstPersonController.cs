390345c4ec1be1c9ba8a28072e5b70d3e962ffea
jacobhan <jungwoohoo@gmail.com>
2015-12-17 00:26:47 -0800

    Update FirstPersonController.cs

--- FirstPersonController.cs
+++ FirstPersonController.cs
@@ -8,7 +8,9 @@ namespace UnityStandardAssets.Characters.FirstPerson {
   [RequireComponent(typeof (CharacterController))]
   [RequireComponent(typeof (AudioSource))]
   public class FirstPersonController : MonoBehaviour {
-    
+    [SerializeField] private bool m_IsWalking;
+    [SerializeField] private float m_WalkSpeed;
+    [SerializeField] private float m_RunSpeed;
     
     
   }
