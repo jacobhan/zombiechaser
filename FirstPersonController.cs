7f41c28033e4c3202dd0c9761c022c2e9464a5cb
jacobhan <jungwoohoo@gmail.com>
2016-01-10 21:11:20 -0800

    Update FirstPersonController.cs

--- FirstPersonController.cs
+++ FirstPersonController.cs
@@ -85,6 +85,9 @@ namespace UnityStandardAssets.Characters.FirstPerson {
     
       m_MoveDir.x = desiredMove.x*speed;
       m_MoveDir.z = desiredMove.z*speed;
+      if (m_CharacterController.isGrounded) {
+        
+      }
     }
   }
   
