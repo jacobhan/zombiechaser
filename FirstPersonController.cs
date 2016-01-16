9b81fc59f8da60ea2d90d93043135c55cfd2b77b
jacobhan <jungwoohoo@gmail.com>
2016-01-15 22:45:51 -0800

    Update FirstPersonController.cs

--- FirstPersonController.cs
+++ FirstPersonController.cs
@@ -108,6 +108,9 @@ namespace UnityStandardAssets.Characters.FirstPerson {
         if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0)) {
           m_StepCycle += (m_CharacterController.velocity.magnitude + (speed*(m_IsWalking ? 1f : m_RunstepLenghten)))*Time.fixedDeltaTime;
         }
+        if (!(m_StepCycle > m_NextStep)) {
+          
+        }
       }
     }
   }
